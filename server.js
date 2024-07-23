f861c32ca62aeea7f8e970c266702254d2c19b32
Dev Mittal <mittal.dev2002@gmail.com>
2024-07-23 21:01:38 +0530

    latest

--- server.js
+++ server.js
@@ -45,16 +45,16 @@ mongoose.connect(URI)
 //     console.log('Connected to MongoDB')
 // })
 
-// if(process.env.NODE_ENV === 'production'){
-//     app.use(express.static('client/build'))
-//     app.get('*', (req, res) => {
-//         res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
-//     })
-// }
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'))
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 
 
 const PORT = process.env.PORT || 5001
 app.listen(PORT, () =>{
-    console.log('Server is running on port', PORT)
+    console.log(`Server is running on port: ${PORT}`)
 })
